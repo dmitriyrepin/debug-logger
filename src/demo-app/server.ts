815e8de8679ae66ba9aaf1b4f867baefb4acd638
Dmitriy Repin <dmitriyrepin@gmail.com>
2018-01-11 23:06:48 +0000

    scale the deployment down

--- src/demo-app/server.ts
+++ src/demo-app/server.ts
@@ -7,5 +7,5 @@ import * as ip from 'ip';
 import { ServerFactory } from './server-factory';
 
 // Get port from environment and store in Express.
-const port = ServerFactory.normalizePort(process.env.PORT || '3000');
+const port = ServerFactory.normalizePort(process.env.PORT || '8080');
 const httpServer = ServerFactory.createAndListen(port);
